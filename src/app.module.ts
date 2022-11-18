ba025137374a575670d73348637af20c805d652c
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2022-11-18 10:11:52 +0300

    set up mongo db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common'
 
+import { MongooseModule, MongooseModuleOptions } from '@nestjs/mongoose'
+
+import Process from 'process'
+
 import { ConfigModule } from '@nestjs/config'
 
 import { AuthModule } from './auth/auth.module'
@@ -7,9 +11,27 @@ import { TopPageModule } from './top-page/top-page.module'
 import { ProductModule } from './product/product.module'
 import { ReviewModule } from './review/review.module'
 
+export const getMongoDbUrl = (process: typeof Process) =>
+  'mongodb://' +
+  process.env.MONGO_DB_LOGIN +
+  ':' +
+  process.env.MONGO_DB_PASSWORD +
+  '@' +
+  process.env.MONGO_DB_HOST +
+  ':' +
+  process.env.MONGO_DB_PORT +
+  '/' +
+  process.env.MONGO_DB_AUTHDATABASE
+
+const mongodbOptions: MongooseModuleOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MongooseModule.forRoot(getMongoDbUrl(process), mongodbOptions),
     AuthModule,
     TopPageModule,
     ProductModule,
