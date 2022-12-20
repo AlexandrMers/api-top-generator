f0c770ac180a92d6e2b5e8e4eac1cd3d97351e98
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2022-12-20 09:00:55 +0300

    add validation id pipe for update top-page

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -58,7 +58,10 @@ export class TopPageController {
   @UsePipes(new ValidationPipe())
   @UseGuards(JwtAuthGuard)
   @Patch(':id')
-  async patch(@Param('id') id: string, @Body() dto: CreateTopPageDto) {
+  async patch(
+    @Param('id', IdValidationPipe) id: string,
+    @Body() dto: CreateTopPageDto,
+  ) {
     const updatedPage = await this.topPageService.updateById(id, dto)
 
     if (!updatedPage) throw new NotFoundException(NOT_FOUND_PAGE_ERROR)
