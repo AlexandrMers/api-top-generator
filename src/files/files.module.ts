0a3b21eda41b4689d4fcd57f04f003bef6b8ffcd
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2022-12-28 08:35:02 +0300

    add static root path for static files

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -8,7 +8,7 @@ import { path } from 'app-root-path'
   imports: [
     ServeStaticModule.forRoot({
       rootPath: `${path}/uploads`,
-      serveRoot: 'static',
+      serveRoot: '/static',
     }),
   ],
   controllers: [FilesController],
