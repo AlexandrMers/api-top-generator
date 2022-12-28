bdf2899d3537709a268cb2e63e351ae19c6a60c8
Aleksandr Avdeev <alexander.avdeev@balance-pl.ru>
2022-12-28 07:45:45 +0300

    add upload files method

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common'
+import {
+  Controller,
+  HttpCode,
+  Inject,
+  Post,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+import { JwtAuthGuard } from '../auth/guards/auth.guard'
+import { FileElementResponse } from './dto/file-element.response'
+import { FilesService } from './files.service'
 
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+  constructor(
+    @Inject(FilesService) private readonly fileService: FilesService,
+  ) {}
+
+  @Post('upload')
+  @HttpCode(200)
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('files'))
+  async uploadFiles(
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<FileElementResponse[]> {
+    return this.fileService.saveFiles([file])
+  }
+}
